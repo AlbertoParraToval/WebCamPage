c808bd2fde658692e859620004daef70b738d44c
Alberto Parra Toval <91873665+AlbertoParraToval@users.noreply.github.com>
2021-12-09 22:17:19 +0100

    Delete javascripts directory

--- javascripts/javascript.js
+++
@@ -1,9 +0,0 @@
-/* BARRA DE NAVEGACION RESPONSIVE*/
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
